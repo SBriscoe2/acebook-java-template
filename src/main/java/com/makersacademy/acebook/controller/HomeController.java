ac3c6cccf776b06ebad88990d8b449c39e6d7d4b
SBriscoe2 <shabris@tiscali.co.uk>
2019-01-15 16:03:39 +0000

    fixed bug on greeting page

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -28,7 +28,7 @@ public class HomeController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "postForm";
+		return "greeting";
 	}
 
 	@GetMapping("/postpage")
